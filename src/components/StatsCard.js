bb1946f561960be379682d6b1b4510c4f5549c9e
KaskaS-O <kaska.salaban@gmail.com>
2022-02-03 11:00:35 +0100

    Create basic mobile layout, update <StatsCard/> structure

--- src/components/StatsCard.js
+++ src/components/StatsCard.js
@@ -1,11 +1,17 @@
 import imgDots from "../images/icon-ellipsis.svg";
+import "../scss/_variables.scss";
 
 const StatsCard = (props) => {
+  const id = `${props.title.replace(" ", "-").toLowerCase()}`;
+  const iconSource = props.icon;
+
   return (
     <div className="card stats">
-      <div className="stats__bar">
-        <img src={props.icon} alt="" aria-hidden="true" />
-      </div>
+      <div
+        className="stats__bar"
+        id={id}
+        style={{ backgroundImage: `url(${iconSource})` }}
+      ></div>
       <div className="stats__data data">
         <h3 className="data__title">{props.title}</h3>
         <span className="data__numbers big">
@@ -15,7 +21,7 @@ const StatsCard = (props) => {
           <img src={imgDots} alt="" />
         </div>
         <span className="data__last-period">
-          Last Week - {props.timeframes.weekly.previous}
+          Last Week - {props.timeframes.weekly.previous}hrs
         </span>
         {/* Uwzglednic zmiany daily/weekly/montly */}
       </div>
